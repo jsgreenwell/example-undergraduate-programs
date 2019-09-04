d48df8d9fdd11397038672cc4f3788b641659177
Josiah S Greenwell <jsgreenwell83@gmail.com>
2019-09-04 13:24:50 -0400

    cleaned it up, added default button, added javadocs

--- OOP/InProgress/JavaFXFormsLecture2/src/loginMVCsample/Controllers/MainController.java
+++ OOP/InProgress/JavaFXFormsLecture2/src/loginMVCsample/Controllers/MainController.java
@@ -2,9 +2,9 @@ package loginMVCsample.Controllers;
 
 import java.util.HashMap;
 import java.util.Map;
-import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import loginMVCsample.Models.LoginModel;
 
 public class MainController {
@@ -14,8 +14,8 @@ public class MainController {
   // This Map will change to a database after H2 introduction
   // This is not the way to build a Map in Java 9+
   private Map<String, String> users = new HashMap<String, String>() {{
-    put("greenwell", "cop3003");
-    put("samuel", "otherPassword");
+      put("greenwell", "cop3003");
+      put("samuel", "otherPassword");
   }};
 
   @FXML
@@ -27,21 +27,28 @@ public class MainController {
 
   }
 
+  /***
+   * <h3>Event for Login Screen - on login button press.</h3>
+   *
+   * @param signinClicked Currently not used but leaving for later
+   * @throws Exception Added in later iteration
+   */
   @FXML
   private void getLogin(ActionEvent signinClicked) throws Exception {
     String user = username.getText().trim();
     String pass = password.getText().trim();
 
-    if ((user != null && pass != null) && (!user.isEmpty() && !pass.isEmpty())){
+    // Technically user & pass should never be "null" but checking doesn't hurt
+    if ((user != null && pass != null) && (!user.isEmpty() && !pass.isEmpty())) {
       String validUser = users.get(user.toLowerCase());
 
-      if (validUser != null && pass.equals(validUser)) {
-        lm.goodLogin(signinClicked);
+      if (pass.equals(validUser)) {
+        lm.goodLogin();
       } else {
-        lm.badLogin(signinClicked);
+        lm.badLogin();
       }
     } else {
-        lm.badLogin(signinClicked);
+      lm.badLogin();
     }
 
   }
