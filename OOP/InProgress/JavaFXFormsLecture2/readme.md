47737595afad46e3615945ae699d79e1e5c32888
Josiah S Greenwell <jsgreenwell83@gmail.com>
2019-09-04 11:52:35 -0400

    added style sheet - took two commands

--- OOP/InProgress/JavaFXFormsLecture2/readme.md
+++ OOP/InProgress/JavaFXFormsLecture2/readme.md
@@ -7,3 +7,9 @@ Setup in a very basic MVC way (only single main view &
 a single "success view"). Typically this would be
 expanded to add better verification, error checking,
 and database checking of username in Model.
+
+### License
+MIT License
+
+### Author
+Josiah Greenwell
--- OOP/InProgress/JavaFXFormsLecture2/src/loginMVCsample/Models/LoginModel.java
+++ OOP/InProgress/JavaFXFormsLecture2/src/loginMVCsample/Models/LoginModel.java
@@ -1,16 +1,12 @@
 package loginMVCsample.Models;
 
-import java.util.Optional;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
-import javafx.stage.Stage;
+
 
 public class LoginModel {
 
