7ad7f675535437917c8c64de0753fc796d86a6f2
Josiah S Greenwell <jsgreenwell83@gmail.com>
2019-12-23 11:48:45 -0500

    Final add for last semester, then clean up, then new programs

---
+++ OOP/InProgress/CompleteProjectExample/src/module-info.java
@@ -0,0 +1,8 @@
+module CompleteProjectExample.FX {
+  requires javafx.fxml;
+  requires javafx.controls;
+  requires javafx.graphics;
+  requires javafx.base;
+
+  opens product;
+}
--- OOP/InProgress/FinalProjectFX8/src/production/Controllers/Controller.java
+++ OOP/InProgress/FinalProjectFX8/src/production/Controllers/Controller.java
@@ -1,6 +1,6 @@
 package production.Controllers;
 
-import javafx.event.ActionEvent;
+//import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
